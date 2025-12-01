4e5c7d4eafa0263110031aa70c4bcb9bd4541e7e
Brayan0313 <69543843+Brayan0313@users.noreply.github.com>
2025-12-01 08:38:21 -0500

    Intentos LAB 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,16 +1,21 @@
+import csv
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
 datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
-
-
 def pregunta_01():
-    """
-    Retorne la suma de la segunda columna.
+    route = "files/input/data.csv"
+    # Almacena todos los valores de la columna 2
+    suma = []
+    with open(route, 'r', encoding='utf-8') as archivo:
+        lector_csv = csv.reader(archivo, delimiter='\t')
+        for fila in lector_csv:
+            # Añade los valores de la columna 2 y los convierte en enteros
+            suma.append(int(fila[1]))
 
-    Rta/
-    214
-
-    """
+        # Retorna resultado
+        suma = sum(suma)
+        
+        return suma
--- homework/pregunta_04.py
+++ homework/pregunta_04.py
@@ -6,23 +6,26 @@ utilizar pandas, numpy o scipy.
 """
 
 
+import csv
+
 def pregunta_04():
-    """
-    La columna 3 contiene una fecha en formato `YYYY-MM-DD`. Retorne la
-    cantidad de registros por cada mes, tal como se muestra a continuación.
+    route = "files/input/data.csv"
+    # Diccionario que almacena los registros
+    dic = {}
+    with open(route, 'r', encoding='utf-8') as archivo:
+        lector_csv = csv.reader(archivo, delimiter='\t')
+        for fila in lector_csv:
+            # Obtiene la fecha y separa el mes
+            fecha = fila[2].split('-')
+            mes = fecha[1]
 
-    Rta/
-    [('01', 3),
-     ('02', 4),
-     ('03', 2),
-     ('04', 4),
-     ('05', 3),
-     ('06', 3),
-     ('07', 5),
-     ('08', 6),
-     ('09', 3),
-     ('10', 2),
-     ('11', 2),
-     ('12', 3)]
+            if mes in dic:
+                dic[mes] += 1
+            else:
+                dic[mes] = 1
 
-    """
+    # Se obtienen los objetos del dic, y se ordenan en base a su letra
+    lista = list(dic.items())
+    lista.sort()
+    
+    return lista
